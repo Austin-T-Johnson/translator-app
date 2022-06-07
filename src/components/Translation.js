a09a2d3dc23f771df93dd15d4c1b13397e3040b4
Austin Johnson <austintjohnson92@gmail.com>
2022-06-06 17:24:18 -0700

    respones from API appears on page

--- src/components/Translation.js
+++ src/components/Translation.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 function Translation(props) {
 
+    const [translation, setTranslation] = useState("")
+
     const options = {
         method: 'GET',
         url: 'https://translated-mymemory---translation-memory.p.rapidapi.com/api/get',
-        params: { langpair: 'en|es', q: 'this shit is straight up fire', mt: '1', onlyprivate: '0', de: 'a@b.c' },
+        params: { langpair: 'en|es', q: 'Max is the man!!', mt: '1', onlyprivate: '0', de: 'a@b.c' },
         headers: {
             'X-RapidAPI-Host': 'translated-mymemory---translation-memory.p.rapidapi.com',
             'X-RapidAPI-Key': '85f2f02d04msh465340af7983dbep158beejsnc533b3ca83f5'
@@ -15,14 +17,14 @@ function Translation(props) {
 
     axios.request(options).then(function (response) {
         console.log(response.data.responseData.translatedText);
-        return response.data.responseData.translatedText
+        setTranslation(response.data.responseData.translatedText) 
     }).catch(function (error) {
         console.error(error);
     });
     return (
         <div>
 
-            {/* <h2>{props.response.data.responseData.translatedText}</h2> */}
+            <h2>{translation}</h2>
 
         </div>
     )
