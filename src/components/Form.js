db10a310e1545936e80a8dda7ebac3a960f6368a
Austin Johnson <austintjohnson92@gmail.com>
2022-06-09 01:09:51 -0700

    started styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -24,8 +24,8 @@ function Form() {
     const [goto, setGoto] = useState();
     const [voiceCode, setVoiceCode] = useState();
     const [audioSrc, setaudioSrc] = useState()
-    useEffect(()=>{
-       // elementRef.current.play();
+    useEffect(() => {
+        // elementRef.current.play();
     }, [audioSrc])
 
     const [sequence, setSequence] = useState({
@@ -46,14 +46,14 @@ function Form() {
     const selectLanguage = (e) => {
         console.log('EVENT TARGET:', e.target.value)
         setLanguage(e.target.value);
-       
+
     }
     const setDexLanguage = () => {
         setIsStopped(false);
         setHasDexLang(true)
     }
-    
-   
+
+
     const animate = () => {
         setGoto({
             value: 114,
@@ -73,19 +73,20 @@ function Form() {
                 'X-RapidAPI-Key': '85f2f02d04msh465340af7983dbep158beejsnc533b3ca83f5'
             }
         };
-        
+
         if (language == 'en') {
             let translation = await getTextToVoice(getDexLangTranslation());
-                setaudioSrc(translation.data.audio_file);
+            setaudioSrc(translation.data.audio_file);
+            setTranslation(getDexLangTranslation())
         } else {
             axios.request(options).then(async (response) => {
                 setTranslation(response.data.responseData.translatedText);
-                
+
                 let translation = await getTextToVoice(response.data.responseData.translatedText);
-               
+
                 setaudioSrc(translation.data.audio_file);
-                
-                
+
+
             }).catch(function (error) {
                 console.error(error);
             });
@@ -94,55 +95,71 @@ function Form() {
     }
 
     const getTextToVoice = (text) => {
-        // const encodedParams = new URLSearchParams();
-        // encodedParams.append("voice_code", `${language}`);
-        // encodedParams.append("text", `${text}`);
-        // encodedParams.append("speed", "1.00");
-        // encodedParams.append("pitch", "1.00");
-        // encodedParams.append("output_type", "audio_url");
-
-        // const options = {
-        //     method: 'POST',
-        //     url: 'https://cloudlabs-text-to-speech.p.rapidapi.com/synthesize',
-        //     headers: {
-        //         'content-type': 'application/x-www-form-urlencoded',
-        //         'X-RapidAPI-Host': 'cloudlabs-text-to-speech.p.rapidapi.com',
-        //         'X-RapidAPI-Key': '85f2f02d04msh465340af7983dbep158beejsnc533b3ca83f5'
-        //     },
-        //     data: encodedParams
-        // };
 
 
-
-        return axios.post('/api/test', {text, language})
+        return axios.post('/api/test', { text, language })
     }
 
     const onPlay = async () => {
         elementRef.current.play();
         //elementRef.src = url;
-        
-        
+    }
+
+    const renderImg = () => {
+        let selected = document.getElementById("selectOption");
+        let imgUrl = ""
+        if (selected.value == "es") {
+            imgUrl = 'https://www.dropbox.com/s/36eojdo0dlqkbgv/Beach-of-Cancun.jpeg?raw=1'
+        } else if (selected.value == "it") {
+            imgUrl = 'https://www.dropbox.com/s/33nfoqswh76ucqr/italy.jpeg?raw=1'
+        } else if (selected.value == "fr") {
+            imgUrl = "https://www.dropbox.com/s/nce1709wc2y9z5n/france.jpeg?raw=1"
+        } else if (selected.value == "de") {
+            imgUrl = "https://www.dropbox.com/s/yf1q32po0yn2w23/germany.jpeg?raw=1"
+        } else if (selected.value == "ja") {
+            imgUrl = "https://www.dropbox.com/s/6asdqkfeg2b85kb/japan.jpeg?raw=1"
+        } else if (selected.value == "no") {
+            imgUrl = "https://www.dropbox.com/s/38lbm4n9g3dp85p/norway.jpeg?raw=1"
+        } else if (selected.value == "pt") {
+            imgUrl = "https://www.dropbox.com/s/ee9qki6u87bepc6/portugal.jpeg?raw=1"
+        } else if (selected.value == "cs") {
+            imgUrl = "https://www.dropbox.com/s/6v4sd4biawar6ti/czech_republic1.jpeg?raw=1"
+        } else if (selected.value == "en") {
+            imgUrl = "https://www.dropbox.com/s/z3oaxles95qizwb/IMG_2574.jpg?raw=1"
+        } else {
+            imgUrl = ""
+        }
+        document.getElementById("myImg").src = imgUrl;
+    }
+
+    const twoCalls = (e) => {
+        renderImg();
+        selectLanguage(e);
     }
 
     return (
         <div>
             <header className="App-header">
-                <h1>Translate Some Stuff</h1>
-            </header>
+              
+                <h1>What do you wanna translate bruh?</h1>
+                  
+               </header>
+
             <form id="form" onSubmit={onSubmit}>
-                <h2>What do you wanna translate bruh?</h2>
-                <input name="input" onChange={onChange} type="text" placeholder='write here bruh'></input>
+
+                {/* <input name="input" onChange={onChange} type="text" placeholder='write here bruh'></input> */}
+                <input onChange={onChange} className="input" type="text" name="input" placeholder="write here bruh"></input>
             </form>
             <br></br>
             <h2>What language bruh?</h2>
             <audio src={audioSrc} ref={elementRef}></audio>
-            <select name="languages" onChange={selectLanguage}>
+            <select name="languages" onChange={twoCalls} id="selectOption">
+                <option value="">Pick a language bruh</option>
                 <option value="es">Spanish</option>
                 <option value="it">Italian</option>
                 <option value="fr">French</option>
                 <option value="de">German</option>
                 <option value="ja">Japanese</option>
-                <option value="la">Latin</option>
                 <option value="no">Norwegian</option>
                 <option value="pt">Portuguese</option>
                 <option value="cs">Czech</option>
@@ -151,11 +168,20 @@ function Form() {
             </select>
             <br></br>
             <br></br>
-            <button onClick={onSubmit}>Translate it bruh</button>
+            {/* <button onClick={onSubmit}>Translate it bruh</button> */}
+            <button onClick={onSubmit}>Translate it bruh<div class="arrow-wrapper"><div class="arrow"></div></div></button>
             <br></br>
-            <h2>{translation}</h2>
+            {translation ? <div class="card"><h2>{translation}</h2></div> : null}
+            
             <br></br>
-            <i id="speaker" className="fa-solid fa-volume-high" onClick={onPlay}></i>
+            {/* <i id="speaker" className="fa-solid fa-volume-high" onClick={onPlay}></i> */}
+            <div>
+                <button id="speaker" onClick={onPlay}></button>
+            </div>
+            <br></br>
+            <img src='imgUrl' id='myImg'></img>
+            <video id="vid" src="https://www.dropbox.com/s/evg0a5bjcwzkqfd/glowing-question-marks-SBV-300077975-HD.mp4?raw=1" muted loop autoPlay></video>
+
             <div onClick={() => animate()} className="easter-egg-btn ">
                 <Lottie options={defaultOptions}
                     height={100}
@@ -163,7 +189,9 @@ function Form() {
                     isStopped={isStopped}
                     goToAndPlay={goto}
                 />
-               
+
+
+
             </div>
         </div>
     )
--- src/helpers/getTranslation.js
+++ src/helpers/getTranslation.js
@@ -1,5 +1,5 @@
 export default function(){
-    let arr = ['My Penis is Hard', 'My Penis is REALLY HARD', 'My Hard Penis is Itchy', 'Itch My Penis', 'My Penis is Very Small and Itchy','VIZZZKAAKAA', 'dvvvvvvvvvvvvvt'];
+    let arr = ['My Penis is Hard', 'My Penis is REALLY HARD', 'My Hard Penis is Itchy', 'Itch My Penis', 'My Penis is Very Small and Itchy','VIZZZKAAKAA', 'dvvvvvvvvvvvvvt', 'Dex has the smallest penis in the entire world', 'Smell my penis'];
     function randomIntFromInterval(min, max) { // min and max included 
         return Math.floor(Math.random() * (max - min + 1) + min)
     }
