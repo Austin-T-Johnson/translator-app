1f20dcb43775a37f4df3ae15f71e91355432eaf1
Austin Johnson <99052077+Austin-T-Johnson@users.noreply.github.com>
2022-06-28 09:21:07 -0700

    Delete getTranslation.js

--- src/helpers/getTranslation.js
+++
@@ -1,8 +0,0 @@
-export default function(){
-    let arr = ['My Penis is Hard', 'My Penis is REALLY HARD', 'My Hard Penis is Itchy', 'Itch My Penis', 'My Penis is Very Small and Itchy','VIZZZKAAKAA', 'dvvvvvvvvvvvvvt'];
-    function randomIntFromInterval(min, max) { // min and max included 
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-    return arr[randomIntFromInterval(0, arr.length-1)]
-
-}
