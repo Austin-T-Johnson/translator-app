7191ee8874f84e15d69362c9064b22a2490a672f
Austin Johnson <austintjohnson92@gmail.com>
2022-07-11 08:51:03 -0700

    server stuff

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 var app = require("./app");
 const port = process.env.PORT || 3001;
-const server = http.createServer(app);
+const server = http.createServer(app) || http.createServer('https://translator-app-two.vercel.app/');
 console.log("running on http://localhost/" + port);
 server.listen(port);
 
