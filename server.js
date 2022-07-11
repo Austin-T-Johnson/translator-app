c718187c5f896a356f610c01babce12bc64ea2cf
Austin Johnson <austintjohnson92@gmail.com>
2022-07-11 08:53:58 -0700

    ff

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 var app = require("./app");
 const port = process.env.PORT || 3001;
-const server = http.createServer(app) || http.createServer('https://translator-app-two.vercel.app/');
+const server = http.createServer(app) || http.createServer('https://translator-app-two.vercel.app/api/test');
 console.log("running on http://localhost/" + port);
 server.listen(port);
 
